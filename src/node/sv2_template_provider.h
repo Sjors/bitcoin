bf413af6ae86199e1be25e87129c0ab789edc54a
Sjors Provoost <sjors@sprovoost.nl>
2024-02-07 14:21:04 +0100

    test: Sv2TemplateProvider tests

--- src/node/sv2_template_provider.h
+++ src/node/sv2_template_provider.h
@@ -234,6 +234,25 @@ public:
      */
     void ProcessMaybeSv2Handshake(Sv2Client& client, Span<std::byte> buffer);
 
+    /** Number of clients that are not marked for disconnection, used for tests. */
+    size_t ConnectedClients() EXCLUSIVE_LOCKS_REQUIRED(m_clients_mutex)
+    {
+        return std::count_if(m_sv2_clients.begin(), m_sv2_clients.end(), [](const auto& c) {
+            return !c->m_disconnect_flag;
+        });
+    }
+
+    /** Number of clients with m_setup_connection_confirmed, used for tests. */
+    size_t FullyConnectedClients() EXCLUSIVE_LOCKS_REQUIRED(m_clients_mutex)
+    {
+        return std::count_if(m_sv2_clients.begin(), m_sv2_clients.end(), [](const auto& c) {
+            return !c->m_disconnect_flag && c->m_setup_connection_confirmed;
+        });
+    }
+
+    /* Block templates that connected clients may be working on */
+    BlockTemplateCache& GetBlockTemplates() EXCLUSIVE_LOCKS_REQUIRED(m_tp_mutex) { return m_block_template_cache; }
+
 private:
     void Init(const Sv2TemplateProviderOptions& options);
 
