37fb62c3a560f4cf559e4a21e3e45dc7201be411
Ryan Ofsky <ryan@ofsky.org>
2025-02-03 09:23:54 -0500

    depends: Update libmultiprocess library before converting to subtree
    Bring in a few cmake changes to improve cmake support
    
    https://github.com/chaincodelabs/libmultiprocess/pull/140 build: don't clobber user/superproject c++ version
    https://github.com/chaincodelabs/libmultiprocess/pull/142 build: add option for external mpgen binary
    https://github.com/chaincodelabs/libmultiprocess/pull/143 cleanup: initialize vars in the EventLoop constructor in the correct order
    https://github.com/chaincodelabs/libmultiprocess/pull/146 cmake: Suppress compiler warnings from capnproto headers
    https://github.com/chaincodelabs/libmultiprocess/pull/147 cmake: EXTERNAL_MPGEN cleanups
    https://github.com/chaincodelabs/libmultiprocess/pull/148 util: fix -Wpessimizing-move warning

--- depends/packages/native_libmultiprocess.mk
+++ depends/packages/native_libmultiprocess.mk
@@ -1,8 +1,8 @@
 package=native_libmultiprocess
-$(package)_version=07c917f7ca910d66abc6d3873162fc9061704074
+$(package)_version=477405eda34d923bd2ba6b3abc4c4d31db84c3ea
 $(package)_download_path=https://github.com/chaincodelabs/libmultiprocess/archive
 $(package)_file_name=$($(package)_version).tar.gz
-$(package)_sha256_hash=ac9db311e3b22aac3c7b7b7b3f6b7fee5cf3043ebb3c3bf412049e8b17166de8
+$(package)_sha256_hash=a62db362fdd95a49c09bd7ac829990d99e937af4300c583772f46e1b3f8d7b43
 $(package)_dependencies=native_capnp
 
 define $(package)_config_cmds
