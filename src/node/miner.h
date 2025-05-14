2dae1cb16e3e32aa477713b88adcfbb6251cc779
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2025-05-14 10:02:21 +0200

    interfaces: refactor: move `waitTipChanged` implementation to miner
    - This commit creates a function `WaitTipChanged` that waits for the connected
      tip to change until timeout elapsed.
    
    - This function is now used by `waitTipChanged`
    
    Co-authored-by: Ryan Ofsky <ryan@ofsky.org>

--- src/node/miner.h
+++ src/node/miner.h
@@ -251,6 +251,9 @@ std::unique_ptr<CBlockTemplate> WaitAndCreateNewBlock(ChainstateManager& chainma
 
 std::optional<BlockRef> GetTip(ChainstateManager& chainman);
 
+/* Waits for the connected tip to change until timeout has elapsed. During node initialization, this will wait until the tip is connected (regardless of `timeout`).
+ * Returns the current tip hash, or nullopt if the node is shutting down. */
+std::optional<BlockRef> WaitTipChanged(ChainstateManager& chainman, KernelNotifications& kernel_notifications, const uint256& current_tip, MillisecondsDouble& timeout);
 } // namespace node
 
 #endif // BITCOIN_NODE_MINER_H
