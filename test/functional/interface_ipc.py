b03feedadb411e15967659be83940d73ce977848
Sjors Provoost <sjors@sprovoost.nl>
2025-11-03 11:08:59 +0100

    [miner] omit dummy extraNonce via IPC
    Previously the coinbase transaction generated by our miner code was
    not used downstream, because the getblocktemplate RPC excludes it.
    
    Since the Mining IPC interface was introduced in #30200 we do expose
    this dummy coinbase transaction. In Stratum v2 several parts of it
    are communicated downstream, including the scriptSig.
    
    This commit removes the dummy extraNonce from the coinbase scriptSig
    in block templates requested via IPC. This limits the scriptSig
    to what is essential for consensus (BIP34) and removes the need for
    external mining software to remove the dummy, or even ignore
    the scriptSig we provide and generate it some other way. This
    could cause problems if a future soft fork requires additional
    data to be committed here.
    
    A test is added to verify the new IPC behavior.
    
    It achieves this by introducing an include_dummy_extranonce
    option which defaults to false with all test code updated to
    set it to true. Because this option is not exposed via IPC,
    callers will no longer see it.
    
    The caller needs to ensure that for blocks 1 through 16
    they pad the scriptSig in order to avoid bad-cb-length.
    
    Co-authored-by: Anthony Towns <aj@erisian.com.au>

--- test/functional/interface_ipc.py
+++ test/functional/interface_ipc.py
@@ -8,6 +8,7 @@ from io import BytesIO
 from pathlib import Path
 import shutil
 from test_framework.messages import (CBlock, CTransaction, ser_uint256, COIN)
+from test_framework.script import (CScript, CScriptNum)
 from test_framework.test_framework import (BitcoinTestFramework, assert_equal)
 from test_framework.wallet import MiniWallet
 
@@ -150,6 +151,12 @@ class IPCInterfaceTest(BitcoinTestFramework):
             coinbase = CTransaction()
             coinbase.deserialize(coinbase_data)
             assert_equal(coinbase.vin[0].prevout.hash, 0)
+
+            # No dummy extraNonce in the coinbase scriptSig
+            current_block_height = self.nodes[0].getchaintips()[0]["height"]
+            expected_scriptsig = CScript([CScriptNum(current_block_height + 1)])
+            assert_equal(coinbase.vin[0].scriptSig.hex(), expected_scriptsig.hex())
+
             self.log.debug("Wait for a new template")
             waitoptions = self.capnp_modules['mining'].BlockWaitOptions()
             waitoptions.timeout = timeout
