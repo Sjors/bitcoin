2ba8528f15cf1a2ed65da8f1c60359450b983427
Sjors Provoost <sjors@sprovoost.nl>
2025-12-04 14:26:37 +0100

    mining: store mining args in NodeContext
    Instead of parsing arguments like -blockmaxweight each time a block
    template is generated, do it once in ApplyArgsManOptions().
    
    These arguments are stored in a new struct MiningArgs.
    
    The BlockAssembler::Options struct is removed in favor of passing
    both MiningArgs and BlockCreateOptions.
    
    This disentangles node configuration from client (or test) options.

--- src/node/interfaces.cpp
+++ src/node/interfaces.cpp
@@ -84,6 +84,8 @@ using interfaces::Node;
 using interfaces::WalletLoader;
 using kernel::ChainstateRole;
 using node::BlockAssembler;
+using node::BlockCreateOptions;
+using node::MiningArgs;
 using node::BlockWaitOptions;
 using util::Join;
 
@@ -860,9 +862,9 @@ public:
 class BlockTemplateImpl : public BlockTemplate
 {
 public:
-    explicit BlockTemplateImpl(BlockAssembler::Options assemble_options,
+    explicit BlockTemplateImpl(BlockCreateOptions create_options,
                                std::unique_ptr<CBlockTemplate> block_template,
-                               NodeContext& node) : m_assemble_options(std::move(assemble_options)),
+                               NodeContext& node) : m_create_options(std::move(create_options)),
                                                     m_block_template(std::move(block_template)),
                                                     m_node(node)
     {
@@ -917,8 +919,15 @@ public:
 
     std::unique_ptr<BlockTemplate> waitNext(BlockWaitOptions options) override
     {
-        auto new_template = WaitAndCreateNewBlock(chainman(), notifications(), m_node.mempool.get(), m_block_template, options, m_assemble_options, m_interrupt_wait);
-        if (new_template) return std::make_unique<BlockTemplateImpl>(m_assemble_options, std::move(new_template), m_node);
+        auto new_template = WaitAndCreateNewBlock(chainman(),
+                                                  notifications(),
+                                                  m_node.mempool.get(),
+                                                  m_block_template,
+                                                  options,
+                                                  m_mining_args,
+                                                  m_create_options,
+                                                  m_interrupt_wait);
+        if (new_template) return std::make_unique<BlockTemplateImpl>(m_create_options, std::move(new_template), m_node);
         return nullptr;
     }
 
@@ -927,7 +936,8 @@ public:
         InterruptWait(notifications(), m_interrupt_wait);
     }
 
-    const BlockAssembler::Options m_assemble_options;
+    const MiningArgs m_mining_args;
+    const BlockCreateOptions m_create_options;
 
     const std::unique_ptr<CBlockTemplate> m_block_template;
 
@@ -962,7 +972,7 @@ public:
         return WaitTipChanged(chainman(), notifications(), current_tip, timeout);
     }
 
-    std::unique_ptr<BlockTemplate> createNewBlock(const BlockCreateOptions& options) override
+    std::unique_ptr<BlockTemplate> createNewBlock(BlockCreateOptions options) override
     {
         // Reject too-small values instead of clamping so callers don't silently
         // end up mining with different options than requested. This matches the
@@ -976,10 +986,15 @@ public:
 
         // Ensure m_tip_block is set so consumers of BlockTemplate can rely on that.
         if (!waitTipChanged(uint256::ZERO, MillisecondsDouble::max())) return {};
-
-        BlockAssembler::Options assemble_options{options};
-        ApplyArgsManOptions(*Assert(m_node.args), assemble_options);
-        return std::make_unique<BlockTemplateImpl>(assemble_options, BlockAssembler{chainman().ActiveChainstate(), context()->mempool.get(), assemble_options}.CreateNewBlock(), m_node);
+        ApplyMiningDefaults(m_node.mining_args, options);
+        return std::make_unique<BlockTemplateImpl>(options,
+                                                   BlockAssembler{
+                                                       chainman().ActiveChainstate(),
+                                                       context()->mempool.get(),
+                                                       context()->mining_args,
+                                                       options,
+                                                    }.CreateNewBlock(),
+                                                   m_node);
     }
 
     bool checkBlock(const CBlock& block, const node::BlockCheckOptions& options, std::string& reason, std::string& debug) override
