ce0de329ef11f6269529c4ef27e2a03cdef53a6e
Sjors Provoost <sjors@sprovoost.nl>
2026-06-24 19:53:43 +0200

    mining: add GetTemplateMemoryUsage()
    Calculate the non-mempool memory footprint for template transaction
    references.
    
    Add bench logging to collect data on whether caching or simplified
    heuristics are needed, such as not checking for mempool presence.

--- src/node/block_template_manager.cpp
+++ src/node/block_template_manager.cpp
@@ -5,7 +5,9 @@
 #include <node/block_template_manager.h>
 
 #include <consensus/validation.h>
+#include <core_memusage.h>
 #include <interfaces/types.h>
+#include <logging/timer.h>
 #include <node/kernel_notifications.h>
 #include <node/miner.h>
 #include <node/mining_args.h>
@@ -15,8 +17,10 @@
 #include <validationinterface.h>
 
 #include <algorithm>
+#include <iterator>
 #include <numeric>
 #include <ranges>
+#include <vector>
 
 namespace node {
 
@@ -58,6 +62,37 @@ void BlockTemplateManager::StopTrackingTemplateTransactions(const std::vector<CT
             m_template_tx_refs.erase(ref_count);
         }
     }
+}
+
+size_t BlockTemplateManager::GetTemplateMemoryUsage() const
+{
+    // Copy transaction references so mempool.exists() below can lock
+    // mempool.cs without inverting the CreateNewTemplate() lock order:
+    // mempool.cs before m_mutex.
+    std::vector<CTransactionRef> tx_refs;
+    {
+        LOCK(m_mutex);
+        tx_refs.reserve(m_template_tx_refs.size());
+        std::ranges::copy(m_template_tx_refs | std::views::keys, std::back_inserter(tx_refs));
+    }
+
+    size_t usage_bytes{0};
+    {
+        LOG_TIME_MILLIS_WITH_CATEGORY("Calculate template transaction reference memory footprint", BCLog::BENCH);
+        for (const auto& tx_ref : tx_refs) {
+            if (!Assume(tx_ref)) continue;
+            // mempool.exists() locks mempool.cs each time, which slows down
+            // our calculation. This is preferable to potentially blocking
+            // the node from processing new transactions while this
+            // (non-urgent) calculation is in progress.
+            //
+            // As a side-effect the result is not an accurate snapshot, because
+            // the mempool may change during the loop.
+            if (m_mempool.exists(tx_ref->GetWitnessHash())) continue;
+            usage_bytes += RecursiveDynamicUsage(*tx_ref);
+        }
+    }
+    return usage_bytes;
 }
 
 void BlockTemplateManager::SanityCheck() const
